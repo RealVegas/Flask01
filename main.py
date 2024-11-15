7629757b9f9fa4a11ae103d048cbf25a9bec6435
Вадим Голов <grafit-design@inbox.ru>
2024-11-15 12:53:36 +0500

    Python все полностью выполнено

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def first_site():
+def main_page():
     return render_template('index.html')
+
+
+@app.route('/blog/')
+def blog_page():
+    return render_template('blog.html')
+
+
+@app.route('/contacts/')
+def contacts_page():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
